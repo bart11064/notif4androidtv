38cd4879d099a6d85b962d891ee37ad1605624a6
bart11064 <bart11064@gmail.com>
2018-09-06 14:05:53 +0200

    Update notif4androidtv.class.php

--- core/class/notif4androidtv.class.php
+++ core/class/notif4androidtv.class.php
@@ -19,7 +19,7 @@
 /* * ***************************Includes********************************* */
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
-class template extends eqLogic {
+class notif4androidtv extends eqLogic {
     /*     * *************************Attributs****************************** */
 
 
@@ -106,7 +106,7 @@ class template extends eqLogic {
     /*     * **********************Getteur Setteur*************************** */
 }
 
-class templateCmd extends cmd {
+class notif4androidtvCmd extends cmd {
     /*     * *************************Attributs****************************** */
 
 
